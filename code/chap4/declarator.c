90885779cde721f28049816eb940e1a099d6ee69
shivshankardayal <shivshankar.dayal@gmail.com~>
2010-07-05 11:45:31 -0400

    None

---
+++ code/chap4/declarator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct
+{
+	char one;
+	char two;
+	char th; 
+	char fo;
+	char fi;
+	char six;
+	char seven;
+	char eight;
+	char nine;
+	char ten;
+	char ele;
+	char twe;
+	char thir;
+}X;
+
+int main()
+{
+	X x={0};
+	return 0;
+}
