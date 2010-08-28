a9cbd292a874e77e5e857e70b5529992d34f0fbc
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2010-08-28 12:30:28 +0530

    full oz.pdf

--- code/chap3/backspace.c
+++ code/chap3/backspace.c
@@ -1,7 +1,18 @@
 /*
+ * Copyright (C) 2010, shiv Shankar Dayal
+ * This file is part of Learn Correct programming.
+
+ * This program is free software. All conditions of GNU GPLv3 or later are
+ * applicable.
+
+ * You should have received a copy of the GNU GPLv3 along with this document. If 
+ * not, see <http://www.gnu.org/licenses/>.
+ * Disclaimer: Any damage or loss of data because of programs given in this
+ * will bear no responsibility to author under any circumstances.
+ 
  * Author: Shiv Shankar Dayal
  * Date: July 4th, 2010
- * Description: Demonstration of structure padding and memory alignment.
+ * Description: Backspace escape sequence demo.
  */
 
 #include <stdio.h>
--- code/chap5/recfib.c
+++ code/chap5/recfib.c
@@ -1,4 +1,15 @@
 /*
+ * Copyright (C) 2010, shiv Shankar Dayal
+ * This file is part of Learn Correct programming.
+
+ * This program is free software. All conditions of GNU GPLv3 or later are
+ * applicable.
+
+ * You should have received a copy of the GNU GPLv3 along with this document. If 
+ * not, see <http://www.gnu.org/licenses/>.
+ * Disclaimer: Any damage or loss of data because of programs given in this
+ * will bear no responsibility to author under any circumstances.
+ 
  * Author: Shiv Shankar Dayal
  * Date: July 6th, 2010
  * Description: Iterative version of Fibonacci numbers
